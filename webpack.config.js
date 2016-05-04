963ed0cc6d05be57682b26a66bb448ed57c70c94
yu xuan <yu xuan>
2016-05-04 14:29:06 +0800

    changes log

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+	//页面入口文件配置
+	entry: {
+		bbc: './dev/bbc.js' //手机分时图
+	},
+	output: {
+		filename: './bundle/[name].js'
+	},
+	module: //加载器配置
+	{
+		// loaders: [
+		// 	{
+		// 		test: /\.css$/,
+		// 		loader: 'style!css'
+		// 	}
+		// ]
+	},
+	devtool: 'source-map',
+	resolve: { //解决方案配置
+		root: [
+		    path.resolve('./modules/')
+		  ],
+	    alias: { //模块简称
+	        extend: 'tools/extend',
+	        jsonp: 'tools/jsonp',
+	        datetime: 'tools/datetime'
+	    }
+	}
+};
